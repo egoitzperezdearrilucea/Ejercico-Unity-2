084faaabcdbd439f9b30943087b8e8213ce75bac
Egoitz <egoitz.perezdearrilucea@ikasle.egibide.org>
2023-09-23 16:03:07 +0200

    Game over en colision

--- Ejercico Unity 2/Assets/Scripts/MovimientoCursor.cs
+++ Ejercico Unity 2/Assets/Scripts/MovimientoCursor.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MovimientoCursor : MonoBehaviour
 {
+    
+    public Text texto;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+    
+    //Game over cuando colisione
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Time.timeScale = 0f;
+    
+        texto.text = "Game over";
+        Debug.Log("Game over");
     }
 
     // Update is called once per frame
